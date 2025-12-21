c42e41c23df596b405a0c7bf0f6542d31ffd28c4
Matheus <rmatheusluiz@gmail.com>
2025-12-21 12:14:54 -0300

    chore: implementa o .env e data/ no .gitignore

--- src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/DTO/FuncionariosDTO.java
+++ src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/DTO/FuncionariosDTO.java
@@ -1,4 +1,23 @@
 package dev.java10x.Cadastro_Funcionarios.Funcionarios.DTO;
 
+import dev.java10x.Cadastro_Funcionarios.Tarefas.Model.TarefasModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//CONSTRUTORES
+@NoArgsConstructor
+@AllArgsConstructor
+
+//GETTERS E SETTERS
+@Data
+
 public class FuncionariosDTO {
+
+    private Long id;
+    private String imgUrl;
+    private String nome;
+    private String email;
+    private String cargo;
+    private TarefasModel tarefas;
 }
--- src/main/java/dev/java10x/Cadastro_Funcionarios/Tarefas/DTO/TarefasDTO.java
+++ src/main/java/dev/java10x/Cadastro_Funcionarios/Tarefas/DTO/TarefasDTO.java
@@ -1,4 +1,27 @@
 package dev.java10x.Cadastro_Funcionarios.Tarefas.DTO;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.java10x.Cadastro_Funcionarios.Funcionarios.Model.FuncionariosModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+//CONSTRUTORES
+@NoArgsConstructor
+@AllArgsConstructor
+
+//GETTERS E SETTERS
+@Data
+
 public class TarefasDTO {
+
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private boolean concluida;
+    @JsonIgnore
+    List<FuncionariosModel> funcionarios;
+
 }
