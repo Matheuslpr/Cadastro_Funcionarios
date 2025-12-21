7a481911241343b3f2f3806d3d65d7b0d7670bb4
Matheus <rmatheusluiz@gmail.com>
2025-12-21 14:24:54 -0300

    fix: Correcao na classes Repository

--- src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/Repository/FuncionariosRepository.java
+++ src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/Repository/FuncionariosRepository.java
@@ -1,6 +1,7 @@
 package dev.java10x.Cadastro_Funcionarios.Funcionarios.Repository;
 
+import dev.java10x.Cadastro_Funcionarios.Funcionarios.Model.FuncionariosModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FuncionariosRepository extends JpaRepository<FuncionariosRepository, Long> {
+public interface FuncionariosRepository extends JpaRepository<FuncionariosModel, Long> {
 }
--- src/main/java/dev/java10x/Cadastro_Funcionarios/Tarefas/Repository/TarefasRepository.java
+++ src/main/java/dev/java10x/Cadastro_Funcionarios/Tarefas/Repository/TarefasRepository.java
@@ -1,6 +1,7 @@
 package dev.java10x.Cadastro_Funcionarios.Tarefas.Repository;
 
+import dev.java10x.Cadastro_Funcionarios.Tarefas.Model.TarefasModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TarefasRepository extends JpaRepository<TarefasRepository, Long> {
+public interface TarefasRepository extends JpaRepository<TarefasModel, Long> {
 }
