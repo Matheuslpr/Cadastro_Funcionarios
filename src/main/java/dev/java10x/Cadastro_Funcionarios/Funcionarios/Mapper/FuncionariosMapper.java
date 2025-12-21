0929ad83b36f8f582bd4ac613e607319df36e361
Matheus <rmatheusluiz@gmail.com>
2025-12-21 14:02:51 -0300

    feat: inclui o mapper para a entidade Funcionarios

--- src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/Mapper/FuncionariosMapper.java
+++ src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/Mapper/FuncionariosMapper.java
@@ -1,4 +1,35 @@
 package dev.java10x.Cadastro_Funcionarios.Funcionarios.Mapper;
 
+import dev.java10x.Cadastro_Funcionarios.Funcionarios.DTO.FuncionariosDTO;
+import dev.java10x.Cadastro_Funcionarios.Funcionarios.Model.FuncionariosModel;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class FuncionariosMapper {
+
+    public FuncionariosModel map(FuncionariosDTO funcionariosDTO){
+
+        FuncionariosModel funcionariosModel = new FuncionariosModel();
+        funcionariosModel.setId(funcionariosDTO.getId());
+        funcionariosModel.setImgUrl(funcionariosDTO.getImgUrl());
+        funcionariosModel.setNome(funcionariosDTO.getNome());
+        funcionariosModel.setEmail(funcionariosDTO.getEmail());
+        funcionariosModel.setCargo(funcionariosDTO.getCargo());
+        funcionariosModel.setTarefas(funcionariosDTO.getTarefas());
+
+        return funcionariosModel;
+    }
+    public  FuncionariosDTO map (FuncionariosModel funcionariosModel){
+
+        FuncionariosDTO funcionariosDTO = new FuncionariosDTO();
+        funcionariosDTO.setId(funcionariosDTO.getId());
+        funcionariosDTO.setImgUrl(funcionariosDTO.getImgUrl());
+        funcionariosDTO.setNome(funcionariosDTO.getNome());
+        funcionariosDTO.setEmail(funcionariosDTO.getEmail());
+        funcionariosDTO.setCargo(funcionariosDTO.getCargo());
+        funcionariosDTO.setTarefas(funcionariosDTO.getTarefas());
+
+        return funcionariosDTO;
+    }
 }
