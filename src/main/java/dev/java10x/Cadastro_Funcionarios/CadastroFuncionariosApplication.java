c1154d7eb60b361ffba7fee63c634a17f3ef9e24
Matheus <rmatheusluiz@gmail.com>
2025-12-21 10:21:49 -0300

    first commit

---
+++ src/main/java/dev/java10x/Cadastro_Funcionarios/CadastroFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.Cadastro_Funcionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroFuncionariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/Cadastro_Funcionarios/CadastroFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.Cadastro_Funcionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
