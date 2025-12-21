9140d1678ce98b0d8659d0b7312a2d75d0b8ff2d
Matheus <rmatheusluiz@gmail.com>
2025-12-21 11:57:16 -0300

    feat: Inclui a annotation ToString, Lombok, jsonIgnore e Entity, além de comecar a modelagem do banco de dados dos models Funcionarios e tarefas

--- src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/Model/FuncionariosModel.java
+++ src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/Model/FuncionariosModel.java
@@ -1,4 +1,43 @@
 package dev.java10x.Cadastro_Funcionarios.Funcionarios.Model;
 
+import dev.java10x.Cadastro_Funcionarios.Tarefas.Model.TarefasModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+//TABELA
+@Entity
+@Table(name = "tb_funcionarios")
+
+//CONSTRUTORES
+@NoArgsConstructor
+@AllArgsConstructor
+
+//GETTERS E SETTERS
+@Data
+
+//ToString
+@ToString(exclude = "tarefas")
+
 public class FuncionariosModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column (name = "img_url")
+    private String imgUrl;
+    @Column(name = "nome")
+    private String nome;
+    @Column(unique=true)
+    private String email;
+    @Column(name = "cargo")
+    private String cargo;
+
+
+    @ManyToOne
+    @JoinColumn(name = "tarefas_id")
+    private TarefasModel tarefas;
 }
--- src/main/java/dev/java10x/Cadastro_Funcionarios/Tarefas/Model/TarefasModel.java
+++ src/main/java/dev/java10x/Cadastro_Funcionarios/Tarefas/Model/TarefasModel.java
@@ -1,4 +1,44 @@
 package dev.java10x.Cadastro_Funcionarios.Tarefas.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.java10x.Cadastro_Funcionarios.Funcionarios.Model.FuncionariosModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+//TABELA
+@Entity
+@Table(name = "tb_tarefas")
+
+//CONSTRUTORES
+@NoArgsConstructor
+@AllArgsConstructor
+
+//GETTERS E SETTERS
+@Data
+
+//ToString
+@ToString
+
+
 public class TarefasModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "id")
+    private String titulo;
+    @Column(name = "titulo")
+    private String descricao;
+    @Column(name = "concluida")
+    private boolean concluida;
+
+
+    @OneToMany(mappedBy = "tarefas")
+    @JsonIgnore
+    List<FuncionariosModel> funcionarios;
 }
