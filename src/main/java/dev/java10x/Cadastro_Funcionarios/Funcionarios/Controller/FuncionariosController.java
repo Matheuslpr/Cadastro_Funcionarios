22a422ec42fa3f4d6cc0cff02cb3585dd7c0e6cd
Matheus <rmatheusluiz@gmail.com>
2025-12-22 17:49:42 -0300

    build: conecta o banco de dado H2 da minha aplicacao

--- src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/Controller/FuncionariosController.java
+++ src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/Controller/FuncionariosController.java
@@ -1,4 +1,15 @@
 package dev.java10x.Cadastro_Funcionarios.Funcionarios.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Funcionarios")
+
 public class FuncionariosController {
+
+
+
+
 }
