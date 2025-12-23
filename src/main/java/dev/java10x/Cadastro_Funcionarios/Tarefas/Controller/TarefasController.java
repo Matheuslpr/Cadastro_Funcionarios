b6424e6ac24129ff517ab71af2716ecab01b1d62
Matheus <rmatheusluiz@gmail.com>
2025-12-23 17:18:33 -0300

    feat: Cria rotas e respostas das requisicoes

--- src/main/java/dev/java10x/Cadastro_Funcionarios/Tarefas/Controller/TarefasController.java
+++ src/main/java/dev/java10x/Cadastro_Funcionarios/Tarefas/Controller/TarefasController.java
@@ -1,4 +1,73 @@
 package dev.java10x.Cadastro_Funcionarios.Tarefas.Controller;
 
+import dev.java10x.Cadastro_Funcionarios.Tarefas.DTO.TarefasDTO;
+import dev.java10x.Cadastro_Funcionarios.Tarefas.Model.TarefasModel;
+import dev.java10x.Cadastro_Funcionarios.Tarefas.Service.TarefasService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tarefas")
 public class TarefasController {
+
+    final private TarefasService tarefasService;
+
+    public TarefasController(TarefasService tarefasService) {
+        this.tarefasService = tarefasService;
+    }
+
+
+    @PostMapping("/criar")
+    public ResponseEntity<String> criarTarefa(@RequestBody TarefasDTO tarefasDTO){
+        TarefasDTO novaTarefa = tarefasService.criarTarefa(tarefasDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Tarefa criada com sucesso: " + novaTarefa.getTitulo() + (" ID") + novaTarefa.getId());
+    }
+
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<TarefasDTO>> listarTarefa(){
+        List<TarefasDTO> tarefas = tarefasService.listarTarefa();
+        return ResponseEntity.ok(tarefas);
+    }
+
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<?> listarTarefaId(@PathVariable Long id) {
+        TarefasDTO tarefasId = tarefasService.listarTarefaId(id);
+        if (tarefasId != null) {
+            return ResponseEntity.ok(tarefasId);
+        }else {
+          return  ResponseEntity.status(HttpStatus.NOT_FOUND)
+                  .body("Tarefa com ID: " + id + " não existe em nosso sistema");
+        }
+    }
+
+
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<?> alterarTarefa(@PathVariable Long id, @RequestBody TarefasDTO tarefaAtualiizada){
+
+        TarefasDTO tarefasDTO = tarefasService.alterarTarefas(id, tarefaAtualiizada);
+        if (tarefasDTO != null){
+            return ResponseEntity.ok(tarefasDTO);
+        }else {
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Tarefa com ID: " + id + " não existe em nosso sistema");
+        }
+    }
+
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletarTarefa(@PathVariable Long id){
+        if (tarefasService.listarTarefaId(id) != null){
+            tarefasService.deletarTarefa(id);
+            return ResponseEntity.ok("Tarefa com ID: " + id + " deletada com sucesso");
+        }else {
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Tarefa com ID: " + id + " não existe em nosso sistema");
+        }
+    }
 }
--- src/main/java/dev/java10x/Cadastro_Funcionarios/Tarefas/Service/TarefasService.java
+++ src/main/java/dev/java10x/Cadastro_Funcionarios/Tarefas/Service/TarefasService.java
@@ -1,4 +1,67 @@
 package dev.java10x.Cadastro_Funcionarios.Tarefas.Service;
 
+import dev.java10x.Cadastro_Funcionarios.Tarefas.DTO.TarefasDTO;
+import dev.java10x.Cadastro_Funcionarios.Tarefas.Mapper.TarefasMapper;
+import dev.java10x.Cadastro_Funcionarios.Tarefas.Model.TarefasModel;
+import dev.java10x.Cadastro_Funcionarios.Tarefas.Repository.TarefasRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class TarefasService {
+
+    final private TarefasRepository tarefasRepository;
+    final private TarefasMapper tarefasMapper;
+
+    public TarefasService(TarefasRepository tarefasRepository, TarefasMapper tarefasMapper) {
+        this.tarefasRepository = tarefasRepository;
+        this.tarefasMapper = tarefasMapper;
+    }
+
+    public List<TarefasDTO> listarTarefa(){
+        List<TarefasModel> tarefas = tarefasRepository.findAll();
+        return tarefas.stream()
+                .map(tarefasMapper::map)
+                .collect(Collectors.toList());
+    }
+
+    public TarefasDTO listarTarefaId(Long id){
+        Optional<TarefasModel> tarefasId = tarefasRepository.findById(id);
+        return tarefasId.map(tarefasMapper::map).orElse(null);
+    }
+
+    public TarefasDTO criarTarefa(TarefasDTO tarefasDTO){
+        TarefasModel tarefas = tarefasMapper.map(tarefasDTO);
+        tarefas = tarefasRepository.save(tarefas);
+        return tarefasMapper.map(tarefas);
+    }
+
+
+    public void deletarTarefa(Long id){
+        tarefasRepository.deleteById(id);
+    }
+
+    public TarefasDTO alterarTarefas(Long id, TarefasDTO tarefasDTO){
+        Optional<TarefasModel> tarefaExistente = tarefasRepository.findById(id);
+        if(tarefaExistente.isPresent()){
+            TarefasModel tarefaAtualizada = tarefasMapper.map(tarefasDTO);
+            tarefaAtualizada.setId(id);
+            TarefasModel tarefaSalva = tarefasRepository.save(tarefaAtualizada);
+            return tarefasMapper.map(tarefaSalva);
+        }
+        return null;
+    }
+
+
+
+
+
+
+
+
 }
+
+
