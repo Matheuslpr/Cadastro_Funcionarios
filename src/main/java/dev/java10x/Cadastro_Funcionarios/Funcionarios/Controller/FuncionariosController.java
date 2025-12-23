31cd1bb7a0046d0c36db6f47bc594951f8445743
Matheus <rmatheusluiz@gmail.com>
2025-12-23 17:18:47 -0300

    feat: Cria rotas e respostas das requisicoes

--- src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/Controller/FuncionariosController.java
+++ src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/Controller/FuncionariosController.java
@@ -1,15 +1,78 @@
 package dev.java10x.Cadastro_Funcionarios.Funcionarios.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.java10x.Cadastro_Funcionarios.Funcionarios.DTO.FuncionariosDTO;
+import dev.java10x.Cadastro_Funcionarios.Funcionarios.Service.FuncionariosService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
-@RequestMapping("/Funcionarios")
-
+@RequestMapping("/funcionarios")
 public class FuncionariosController {
 
 
+    final private FuncionariosService funcionariosService;
+
+    public FuncionariosController(FuncionariosService funcionariosService) {
+        this.funcionariosService = funcionariosService;
+    }
+
+
+    @PostMapping("/criar")
+    public ResponseEntity<String> criarFuncionario(@RequestBody FuncionariosDTO funcionariosDTO){
+        FuncionariosDTO novoFuncionaio = funcionariosService.criarFuncionario(funcionariosDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Funcionario criado com sucesso: " + novoFuncionaio.getNome() + (" ID") + novoFuncionaio.getId());
+    }
+
+
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<FuncionariosDTO>> listarFuncionarios(){
+        List<FuncionariosDTO> funcionarios = funcionariosService.listarFuncionarios();
+        return ResponseEntity.ok(funcionarios) ;
+    }
+
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<?> listarFuncionariosId(@PathVariable Long id){
+        FuncionariosDTO funcionariosId = funcionariosService.listarFuncionariosId(id);
+        if(funcionariosId != null){
+            return ResponseEntity.ok(funcionariosId);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Funcionario com ID: " + id + " não existe em nosso sistema");
+        }
+    }
+
+
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<?> alterarFincionarios(@PathVariable Long id, @RequestBody FuncionariosDTO funcionarioAtualizado){
+
+        FuncionariosDTO funcionariosDTO = funcionariosService.alterarFuncionarios(id, funcionarioAtualizado);
+        if(funcionariosDTO != null){
+            return ResponseEntity.ok(funcionariosDTO);
+        } else {
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Funcionario com ID: " + id + " não existe em nosso sistema");
+        }
+
+    }
+
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletarFuncionario(@PathVariable Long id){
+        if (funcionariosService.listarFuncionariosId(id) != null){
+            funcionariosService.deletarFuncionario(id);
+            return ResponseEntity.ok("Funcionario com ID " + id + " deletado com sucesso");
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Não foi possivel encontrar Funcionario com ID: " + id);
+
+        }
+    }
 
 
 }
+
--- src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/Service/FuncionariosService.java
+++ src/main/java/dev/java10x/Cadastro_Funcionarios/Funcionarios/Service/FuncionariosService.java
@@ -1,4 +1,61 @@
 package dev.java10x.Cadastro_Funcionarios.Funcionarios.Service;
 
+import dev.java10x.Cadastro_Funcionarios.Funcionarios.DTO.FuncionariosDTO;
+import dev.java10x.Cadastro_Funcionarios.Funcionarios.Mapper.FuncionariosMapper;
+import dev.java10x.Cadastro_Funcionarios.Funcionarios.Model.FuncionariosModel;
+import dev.java10x.Cadastro_Funcionarios.Funcionarios.Repository.FuncionariosRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class FuncionariosService {
+
+    final private FuncionariosRepository funcionariosRepository;
+    final  private FuncionariosMapper funcionariosMapper;
+
+    public FuncionariosService(FuncionariosRepository funcionariosRepository, FuncionariosMapper funcionariosMapper) {
+        this.funcionariosRepository = funcionariosRepository;
+        this.funcionariosMapper = funcionariosMapper;
+    }
+
+
+    public List<FuncionariosDTO> listarFuncionarios(){
+        List<FuncionariosModel> funcionarios = funcionariosRepository.findAll();
+        return funcionarios.stream()
+                .map(funcionariosMapper::map)
+                .collect(Collectors.toList());
+    }
+
+
+    public FuncionariosDTO listarFuncionariosId(Long id){
+        Optional<FuncionariosModel> funcionariosId = funcionariosRepository.findById(id);
+        return funcionariosId.map(funcionariosMapper::map).orElse(null);
+    }
+
+
+    public FuncionariosDTO criarFuncionario(FuncionariosDTO funcionariosDTO){
+        FuncionariosModel funcionarios = funcionariosMapper.map(funcionariosDTO);
+        funcionarios = funcionariosRepository.save(funcionarios);
+        return funcionariosMapper.map(funcionarios);
+    }
+
+
+    public void deletarFuncionario(Long id){
+        funcionariosRepository.deleteById(id);
+    }
+
+
+    public FuncionariosDTO alterarFuncionarios(Long id, FuncionariosDTO funcionariosDTO){
+        Optional<FuncionariosModel> funcionarioExistente = funcionariosRepository.findById(id);
+        if(funcionarioExistente.isPresent()){
+            FuncionariosModel funcionarioAtualizado = funcionariosMapper.map(funcionariosDTO);
+            funcionarioAtualizado.setId(id);
+            FuncionariosModel funcionarioSalvo = funcionariosRepository.save(funcionarioAtualizado);
+            return funcionariosMapper.map(funcionarioSalvo);
+        }
+        return null;
+    }
 }
